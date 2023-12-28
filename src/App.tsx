8e022df7f605c83dc8f4134b0e495d6232fc4e82
Phalu Yadlapati <phalguna.yadlapati@gmail.com>
2023-12-28 15:32:53 -0500

    initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import { Amplify } from 'aws-amplify';
+import config from './amplifyconfiguration.json'; // Update the path as necessary
+import '@aws-amplify/ui-react/styles.css';
+import HomePage from './Home';
+
+
+
+Amplify.configure(config);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HomePage />
   );
 }
 
-export default App;
+export default withAuthenticator(App);
